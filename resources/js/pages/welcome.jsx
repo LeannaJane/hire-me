ca7321c3127bce9954ca21c830fec5a51f6cc364
Leannaaaa <l.roys-2020@hull.ac.uk>
2025-03-20 23:18:45 +0000

    Creating a project called HIRE ME

---
+++ resources/js/pages/welcome.jsx
@@ -0,0 +1,15 @@
+export default function Welcome() {
+
+    return (
+        <div className="container py-4">
+            <div class="d-flex flex-column align-items-center">
+                <h1 className="">Hire Me</h1>
+                <p className="">A tool to help you stay organised, motivated, and in control of your job applications</p>
+                <img src="https://placehold.co/600x400" alt="Hire Me" />
+                <a href="/register" className="btn btn-light mt-4 px-5 fw-bold">Get Started</a>
+                <p className="mt-3">Already have an account?</p>
+                <a href="/login" className="text-white">Login</a>
+            </div>
+        </div>
+    )
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx', "resources/js/pages/{$page['component']}.jsx"])
+    @inertiaHead
+  </head>
+  <body class="bg-off-black">
+    @inertia
+  </body>
+</html>
