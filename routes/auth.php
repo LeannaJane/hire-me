8eaaa63fc9eb3db37310d6527deb613eb3008ae6
Leannaaaa <l.roys-2020@hull.ac.uk>
2025-03-22 17:06:57 +0000

    Adding middleware, to protect dashboard and other content.

--- routes/auth.php
+++ routes/auth.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/login", [LoginController::class, "view"]);
-Route::get("/register", [RegisterController::class, "view"]);
+Route::get("/login", [LoginController::class, "view"])->name('login');
+Route::get("/register", [RegisterController::class, "view"])->name('register');
 
 Route::post('/register', [RegisterController::class, 'store'])->name('register');
 Route::post('/login', [LoginController::class, 'login']);
+
+// Middleware protects the dashboard and other routes from the dashboard from unauthorised users.
+Route::middleware([/*'auth',*/ 'inertia'])->group(function () {
+    Route::get("/dashboard", [DashboardController::class, "view"]);
+});
