60970da397d7aca68ee2b098eb776e914bfe96ce
Leannaaaa <l.roys-2020@hull.ac.uk>
2025-03-22 22:41:28 +0000

    adding calender using react import

--- resources/js/Layouts/AuthLayout.jsx
+++ resources/js/Layouts/AuthLayout.jsx
@@ -2,12 +2,12 @@ import SidebarControl from "../Components/SidebarControl";
 
 export default function AuthLayout({ routes, children }) {
     return (
-        <div className="d-flex vh-100">
+        <div className="d-flex vh-100 overflow-hidden">
             {/* Sidebar Component */}
             <SidebarControl routes={routes} />
 
             {/* Main Content */}
-            <div className="flex-grow-1 p-4">
+            <div className="flex-grow-1 p-4 overflow-auto">
                 {children}
             </div>
         </div>
--- resources/js/pages/calendar.jsx
+++ resources/js/pages/calendar.jsx
@@ -1,9 +1,11 @@
+import BigCalendar from "../Components/Calendar/BigCalendar";
 import AuthLayout from "../Layouts/AuthLayout";
 
 export default function Calendar({routes, user}) {
     return (
         <AuthLayout routes={routes}>
             <h1>Your Calendar</h1>
+            <BigCalendar></BigCalendar>
         </AuthLayout>
     );
 }
